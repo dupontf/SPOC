79db3f47789294bf8068e456379e900057a08eee
Frédéric Dupont <frederic.dupont88@gmail.com>
2024-10-31 10:50:47 -0400

    add curved noslip elements

--- src/calvarnl_ns.f90
+++ src/calvarnl_ns.f90
@@ -5,6 +5,8 @@
 ! viscosity
 ! beta plane
 ! varying bottom topography
+! curved elements
+! open boudary
 !******************************************************************
 !
 subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
@@ -13,21 +15,28 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
     use gauss
     use utilities
     use boundary_condition
+    use gauss_bd
+    use boundary_bd
     
       implicit none
       double precision u0(nm,ne),v0(nm,ne),h0(nm,ne), &
                        u1(nm,ne),v1(nm,ne),h1(nm,ne),&
                  w0(nm,ne),z0(nm,ne),hb(nm,ne),&
                  dia1(nm),dia2(nm),&
-                 ak(nm),b(nm),bu(nm),bv(nm),&
+                 dia3(nm),dia4(nm), dia5(nm), &
+                 ak(nm),b(nm),bu(nm),&
                  uk(nm),vk(nm),hk(nm),wk(nm),zk(nm),&
                  uhk(nm),vhk(nm),&
                  uux(nm),vuy(nm),&
                  uvx(nm),vvy(nm),&
                  gx(nm),gy(nm),gn(nm),gt(nm),&
                  gx1(nm),gy1(nm),&
-                 ax,by,ay,bx,deta,detai,coeff,dis,dnx,dny,mu,mv,&
+		 amm0(nm,nm),amm1(nm,nm),amm2(nm,nm), &
+		 amm3(nm,nm), amm4(nm,nm), &
+! --- some scalar
+                 ax,by,ay,bx,deta,detai,dis,dnx,dny,mu,mv,&
                  mu0,mb0,fc(nm,ne),g0,time,pi,tday,hh0,&
+! --- some other stuff
                  gxx(nm),gxy(nm),gyx(nm),gyy(nm),&
                  gnx(nm),gtx(nm),gny(nm),gty(nm),&
                  gnn(nm),gtn(nm),gnt(nm),gtt(nm),&
@@ -35,17 +44,27 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
                  qyx(nm,ne),qyy(nm,ne),&
                  fxx(nm,ne),fxy(nm,ne),&
                  fyx(nm,ne),fyy(nm,ne),&
-                 fbdxx(ngf,nf),fbdxy(ngf,nf),&
-                 fbdyx(ngf,nf),fbdyy(ngf,nf),&
-                 fbdx(ngf,nf),fbdy(ngf,nf),&
-                 fbd(ngf,nf),&
                  adu(nm,ne),adv(nm,ne),&
                  uh(nm,ne),vh(nm,ne),&
                  w1(nm,ne),z1(nm,ne),&
-                 mff(ng), mff1(ng), mff2(ng), mff3(ng), mff4(ng)
-      integer i,j,k,l,info,i1,i2,i3,ind,ns1
+! --- some gauss tables
+                 fbdxx(ngf_bd,nf),fbdxy(ngf_bd,nf),&
+                 fbdyx(ngf_bd,nf),fbdyy(ngf_bd,nf),&
+                 fbdx(ngf_bd,nf),fbdy(ngf_bd,nf),&
+                 fbd(ngf_bd,nf),&
+                 mff(ng_bd), mff1(ng_bd), &
+		 mff2(ng_bd), mff3(ng_bd), mff4(ng_bd), &
+		 jac0(ng_bd), jac1(ng_bd), jac2(ng_bd), &
+		 jac11(ng_bd), jac12(ng_bd), &
+		 jac21(ng_bd), jac22(ng_bd), &
+		 ut(ng_bd), un(ng_bd), &
+		 uxx(ng_bd), uxy(ng_bd), uyx(ng_bd), uyy(ng_bd), &
+		 utx(ng_bd), uty(ng_bd), unx(ng_bd), uny(ng_bd), &
+		 utt(ng_bd), utn(ng_bd), unt(ng_bd), unn(ng_bd)
+      integer i,j,k,l,info,i1,i2,i3,ind,ns1,tk
       integer cell,face
 
+      hh0=hb(1,1)
 
       
 !----------- matrix inversion---------------
@@ -65,21 +84,13 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 !
 
 	call cal_flu(fbd,u0)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdxx(j,face) = fbd(j,face)*norm(1,face)
-	   fbdxy(j,face) = fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdxx,1)
+	call norm_flu(fbd,fbdxy,2)
 
 
 	call cal_flu(fbd,v0)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdyx(j,face) = fbd(j,face)*norm(1,face)
-	   fbdyy(j,face) = fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdyx,1)
+	call norm_flu(fbd,fbdyy,2)
 
 
 	do cell=1,ne
@@ -90,49 +101,104 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
          by    = vect(4,cell)
          detai = vect(5,cell)
 
-	   do j=1,nm
-	      uk(j) = u0(j,cell)
-	      vk(j) = v0(j,cell)
-	   enddo
+	      uk = u0(:,cell)
+	      vk = v0(:,cell)
+
+!
+!------------------------------------------------
+! si la cellule est au bord, rotation
+!------------------------------------------------
+!
+         tk=pbdl_inv(cell)
+
+	 if (bdl(cell)) then
+
+	 amm0=amm_bd(:,:,tk)
+	 amm1=amm_bdl(:,:,tk)
+	 amm2=agx_bd(:,:,tk)
+	 amm3=agy_bd(:,:,tk)
+
+         do k=1,nm-nc-1
+          dia4(k) = 1.d0/amm1(k,k)
+         enddo
+         do k=1,nm
+          dia5(k) = 1.d0/amm0(k,k)
+         enddo
 
 !------------------------------------------------
-	   do j=1,nm
-	      gx1(j) =0.d0
-	      gy1(j) =0.d0
-	   enddo
+	      gx1=0.d0
+	      gy1=0.d0
+
+	   call cal_b(gx1,uk,amm2,-1.d0)
+	   call cal_b(gy1,uk,amm3,-1.d0)
+
+	   gxx = (  by * gx1 - ay * gy1) * detai
+	   gxy = (- bx * gx1 + ax * gy1) * detai
+
+           call tri_fc(cell,fbdxx,gxx,detai)
+  	   call tri_fc(cell,fbdxy,gxy,detai)
+
+!------------------------------------------------
+	      gx1=0.d0
+	      gy1=0.d0
+
+	   call cal_b(gx1,vk,amm2,-1.d0)
+	   call cal_b(gy1,vk,amm3,-1.d0)
+
+	   gyx = (  by * gx1 - ay * gy1) * detai
+	   gyy = (- bx * gx1 + ax * gy1) * detai
+
+           call tri_fc(cell,fbdyx,gyx,detai)
+  	   call tri_fc(cell,fbdyy,gyy,detai)
+!
+!------------------------------------------------
+!       rotation des vitesse pour que la vitesse v soit 
+!       normale a la face
+!
+         call dtrsm4(nm,nm,amm0,gxx,dia5)
+         call dtrsm4(nm,nm,amm0,gxy,dia5)
+         call dtrsm4(nm,nm,amm0,gyx,dia5)
+         call dtrsm4(nm,nm,amm0,gyy,dia5)
+
+
+!-------------------------------------------------
+! cas normal sans rotation
+!-------------------------------------------------
+
+	   else
+
+!------------------------------------------------
+	      gx1=0.d0
+	      gy1=0.d0
+
 	   call cal_b(gx1,uk,agx,-1.d0)
 	   call cal_b(gy1,uk,agy,-1.d0)
 
-	   do j=1,nm
-		gxx(j) = (  by * gx1(j) - ay * gy1(j)) * detai
-		gxy(j) = (- bx * gx1(j) + ax * gy1(j)) * detai
-	   enddo
+	   gxx = (  by * gx1 - ay * gy1) * detai
+	   gxy = (- bx * gx1 + ax * gy1) * detai
 
            call tri_f(cell,fbdxx,gxx,detai)
   	   call tri_f(cell,fbdxy,gxy,detai)
 
 !------------------------------------------------
-	   do j=1,nm
-	      gx1(j) =0.d0
-	      gy1(j) =0.d0
-	   enddo
+	      gx1=0.d0
+	      gy1=0.d0
+
 	   call cal_b(gx1,vk,agx,-1.d0)
 	   call cal_b(gy1,vk,agy,-1.d0)
 
-	   do j=1,nm
-		gyx(j) = (  by * gx1(j) - ay * gy1(j)) * detai
-		gyy(j) = (- bx * gx1(j) + ax * gy1(j)) * detai
-	   enddo
+	   gyx = (  by * gx1 - ay * gy1) * detai
+	   gyy = (- bx * gx1 + ax * gy1) * detai
 
            call tri_f(cell,fbdyx,gyx,detai)
   	   call tri_f(cell,fbdyy,gyy,detai)
-
-!------------------------------------------------
 
 	   call dtrsm4(nm,nm,amm,gxx,dia1)
 	   call dtrsm4(nm,nm,amm,gxy,dia1)
 	   call dtrsm4(nm,nm,amm,gyx,dia1)
 	   call dtrsm4(nm,nm,amm,gyy,dia1)
+
+	   endif
 
 	   do j=1,nm
 		qxx(j,cell) = gxx(j)
@@ -154,13 +220,163 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 	      uk(j) = u0(j,cell)
 	      vk(j) = v0(j,cell)
 	      hk(j) = h0(j,cell)+hb(j,cell)
-	      wk(j) = w0(j,cell) - mb0 * uk(j)
-	      zk(j) = z0(j,cell) - mb0 * vk(j)
+	      wk(j) = w0(j,cell)
+	      zk(j) = z0(j,cell)
 	      gxx(j) = qxx(j,cell)
 	      gxy(j) = qxy(j,cell) - fc(j,cell)
 	      gyx(j) = qyx(j,cell) + fc(j,cell)
 	      gyy(j) = qyy(j,cell)
 	   enddo
+
+
+         tk=pbdl_inv(cell)
+
+	 if (fflagt(cell).eq.1) then
+
+           jac0 = jac(:,tk)
+
+	   call spectoxy_c(mff1,uk)
+	   call spectoxy_c(mff2,vk)
+
+	   call spectoxy_c(mff3,gxx)
+	   call spectoxy_c(mff4,gxy)
+
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uux,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff4)
+	   call xytospec_c(vuy,mff,jac0)
+
+	   call spectoxy_c(mff3,gyx)
+	   call spectoxy_c(mff4,gyy)
+
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uvx,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff4)
+	   call xytospec_c(vvy,mff,jac0)
+
+!-------------------------------------------------
+! calcul du flux de masse u*h,v*h
+!
+
+	   call spectoxy_c(mff3,hk)
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uhk,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff3)
+	   call xytospec_c(vhk,mff,jac0)
+!
+! calcul du forcing en u (vent + friction)
+!
+           mff = sqrt( mff1 * mff1 + mff2 * mff2 ) + 1.d-1
+	   call spectoxy_c(mff4,wk)
+	   mff4 = mff4 - mb0 * mff * mff1
+	   call divide(ng_bd,mff,mff4,mff3)
+	   call xytospec_c(wk,mff,jac0)
+!
+! calcul du forcing en v (vent + friction)
+!
+	   call spectoxy_c(mff4,zk)
+	   mff4 = mff4 - mb0 * mff * mff2
+	   call divide(ng_bd,mff,mff4,mff3)
+	   call xytospec_c(zk,mff,jac0)
+
+
+!------------------------------------------------
+!       rotation des vitesse pour que la vitesse v soit 
+!       normale a la face
+
+	 amm0=amm_bd(:,:,tk)
+	 amm1=amm_bdl(:,:,tk)
+
+         do k=1,nm-nc-1
+          dia4(k) = 1.d0/amm1(k,k)
+         enddo
+         do k=1,nm
+          dia5(k) = 1.d0/amm0(k,k)
+         enddo
+
+!------------------------------------------------
+! reduction
+!
+         call dtrsm4(nm,nm,amm0,uhk,dia5)
+         call dtrsm4(nm,nm,amm0,vhk,dia5)
+	 
+         call reduc_gn(uhk,bu)
+         call reduc_gn(vhk,bv)
+         call dtrsm4(nm,nm-nc-1,amm1,bu,dia4)
+         call dtrsm4(nm,nm-nc-1,amm1,bv,dia4)
+         call reduc_inv_gn(uhk,bu)
+         call reduc_inv_gn(vhk,bv)
+
+
+!********* curved element along an open boundary *************
+
+	 else if (fflagt(cell).eq.5) then
+
+           jac0 = jac(:,tk)
+
+	   call spectoxy_c(mff1,uk)
+	   call spectoxy_c(mff2,vk)
+
+	   call spectoxy_c(mff3,gxx)
+	   call spectoxy_c(mff4,gxy)
+
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uux,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff4)
+	   call xytospec_c(vuy,mff,jac0)
+
+	   call spectoxy_c(mff3,gyx)
+	   call spectoxy_c(mff4,gyy)
+
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uvx,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff4)
+	   call xytospec_c(vvy,mff,jac0)
+
+!-------------------------------------------------
+! calcul du flux de masse u*h,v*h
+!
+
+	   call spectoxy_c(mff3,hk)
+	   call multiply(ng_bd,mff,mff1,mff3)
+	   call xytospec_c(uhk,mff,jac0)
+	   call multiply(ng_bd,mff,mff2,mff3)
+	   call xytospec_c(vhk,mff,jac0)
+!
+! calcul du forcing en u (vent + friction)
+!
+           mff = sqrt( mff1 * mff1 + mff2 * mff2 ) + 1.d-1
+	   call spectoxy_c(mff4,wk)
+	   mff4 = mff4 - mb0 * mff * mff1
+	   call divide(ng_bd,mff,mff4,mff3)
+	   call xytospec_c(wk,mff,jac0)
+!
+! calcul du forcing en v (vent + friction)
+!
+	   call spectoxy_c(mff4,zk)
+	   mff4 = mff4 - mb0 * mff * mff2
+	   call divide(ng_bd,mff,mff4,mff3)
+	   call xytospec_c(zk,mff,jac0)
+
+
+!------------------------------------------------
+!       rotation des vitesse pour que la vitesse v soit 
+!       normale a la face
+
+	 amm0=amm_bd(:,:,tk)
+
+         do k=1,nm
+          dia5(k) = 1.d0/amm0(k,k)
+         enddo
+
+
+         call dtrsm4(nm,nm,amm0,uhk,dia5)
+         call dtrsm4(nm,nm,amm0,vhk,dia5)
+
+
+! ******* interior element ********
+
+	else
 
 	   call spectoxy(mff1,uk)
 	   call spectoxy(mff2,vk)
@@ -193,32 +409,19 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 !
 ! calcul du forcing en u (vent + friction)
 !
+           mff = sqrt( mff1 * mff1 + mff2 * mff2 ) + 1.d-1
 	   call spectoxy(mff4,wk)
+	   mff4 = mff4 - mb0 * mff * mff1
 	   call divide(ng,mff,mff4,mff3)
-	   call xytospec(wk,mff)
+	   call xytospec(wk,mff,jac0)
 !
 ! calcul du forcing en v (vent + friction)
 !
 	   call spectoxy(mff4,zk)
+	   mff4 = mff4 - mb0 * mff * mff2
 	   call divide(ng,mff,mff4,mff3)
-	   call xytospec(zk,mff)
+	   call xytospec(zk,mff,jac0)
 
-
-	 if (fflagt(cell).eq.1) then
-
-!------------------------------------------------
-! projection onto the zero-at-the-boundary basis functions
-!
-	   call reduc_gn(uhk,bu)
-	   call reduc_gn(vhk,bv)
-
-	   call dtrsm4(nm-nc-1,amml,bu,dia2)
-	   call dtrsm4(nm-nc-1,amml,bv,dia2)
-
-	   call reduc_inv_gn(uhk,bu)
-	   call reduc_inv_gn(vhk,bv)
-
-	else
 
 	   call dtrsm4(nm,nm,amm,uhk,dia1)
 	   call dtrsm4(nm,nm,amm,vhk,dia1)
@@ -248,29 +451,13 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 	fyy = mu0 * qyy
 
 	call cal_flu(fbd,fxx)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdxx(j,face) = fbd(j,face)*norm(1,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdxx,1)
 	call cal_flu(fbd,fxy)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdxy(j,face) = fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdxy,2)
 	call cal_flu(fbd,fyx)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdyx(j,face) = fbd(j,face)*norm(1,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdyx,1)
 	call cal_flu(fbd,fyy)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdyy(j,face) = fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdyy,2)
 
 	fxx = fxx - g0 * h0
 	fyy = fyy - g0 * h0
@@ -278,12 +465,11 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
         call cal_flu(fbd,h0)
 	call openbc1(fbd,time,1.d0)
 
-	do face=1,nf
-	   do j=1,ngf
-	   fbdxx(j,face) = fbdxx(j,face) - g0 * fbd(j,face)*norm(1,face)
-	   fbdyy(j,face) = fbdyy(j,face) - g0 * fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdx,1)
+	call norm_flu(fbd,fbdy,2)
+	
+	fbdxx = fbdxx - g0 * fbdx
+	fbdyy = fbdyy - g0 * fbdy
 	
 
 	
@@ -308,6 +494,118 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 	      gxy = fxy(:,cell)
 	      gyx = fyx(:,cell)
 	      gyy = fyy(:,cell)
+
+       tk=pbdl_inv(cell)
+
+!
+!------------------------------------------------
+! si la cellule est au bord, noslip
+!------------------------------------------------
+!
+	 if (fflagt(cell).eq.1) then
+	 
+	 
+	 amm0=amm_bd(:,:,tk)
+	 amm1=amm_bdl(:,:,tk)
+	 amm2=agx_bd(:,:,tk)
+	 amm3=agy_bd(:,:,tk)
+
+         do k=1,nm-nc-1
+          dia4(k) = 1.d0/amm1(k,k)
+         enddo
+         do k=1,nm
+          dia5(k) = 1.d0/amm0(k,k)
+         enddo
+
+!------------------------------------------------
+! calcul de tenseur pour u
+!
+         call cal_b(gx,gxx,amm2,-by*detai)
+         call cal_b(gx,gxx,amm3, ay*detai)
+
+         call cal_b(gx,gxy,amm2, bx*detai)
+         call cal_b(gx,gxy,amm3,-ax*detai)
+
+         call tri_fc(cell,fbdxx,gx,detai)
+         call tri_fc(cell,fbdxy,gx,detai)
+
+!------------------------------------------------
+! calcul de tenseur pour v
+!
+         call cal_b(gy,gyx,amm2,-by*detai)
+         call cal_b(gy,gyx,amm3,ay*detai)
+
+         call cal_b(gy,gyy,amm2,bx*detai)
+         call cal_b(gy,gyy,amm3,-ax*detai)
+
+         call tri_fc(cell,fbdyx,gy,detai)
+         call tri_fc(cell,fbdyy,gy,detai)
+
+
+!------------------------------------------------
+! reduction
+!
+         call dtrsm4(nm,nm,amm0,gx,dia5)
+         call dtrsm4(nm,nm,amm0,gy,dia5)
+	 
+         call reduc_gn(gx,bu)
+         call reduc_gn(gy,bv)
+         call dtrsm4(nm,nm-nc-1,amm1,bu,dia4)
+         call dtrsm4(nm,nm-nc-1,amm1,bv,dia4)
+         call reduc_inv_gn(gx,bu)
+         call reduc_inv_gn(gy,bv)
+
+
+!******* open boudanry condition *******
+
+	 else if (fflagt(cell).eq.5) then
+
+
+	 amm0=amm_bd(:,:,tk)
+	 amm2=agx_bd(:,:,tk)
+	 amm3=agy_bd(:,:,tk)
+
+         do k=1,nm-nc-1
+          dia4(k) = 1.d0/amm1(k,k)
+         enddo
+         do k=1,nm
+          dia5(k) = 1.d0/amm0(k,k)
+         enddo
+
+!------------------------------------------------
+! calcul de tenseur pour u
+!
+         call cal_b(gx,gxx,amm2,-by*detai)
+         call cal_b(gx,gxx,amm3, ay*detai)
+
+         call cal_b(gx,gxy,amm2, bx*detai)
+         call cal_b(gx,gxy,amm3,-ax*detai)
+
+         call tri_fc(cell,fbdxx,gx,detai)
+         call tri_fc(cell,fbdxy,gx,detai)
+
+!------------------------------------------------
+! calcul de tenseur pour v
+!
+         call cal_b(gy,gyx,amm2,-by*detai)
+         call cal_b(gy,gyx,amm3,ay*detai)
+
+         call cal_b(gy,gyy,amm2,bx*detai)
+         call cal_b(gy,gyy,amm3,-ax*detai)
+
+         call tri_fc(cell,fbdyx,gy,detai)
+         call tri_fc(cell,fbdyy,gy,detai)
+
+!--- solution
+
+         call dtrsm4(nm,nm,amm0,gx,dia5)
+         call dtrsm4(nm,nm,amm0,gy,dia5)
+
+
+
+! ******* interior element *******
+	   else
+
 !
 !------------------------------------------------
 ! calcul de tenseur pour u
@@ -333,36 +631,6 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 	   call tri_f(cell,fbdyx,gy,detai)
 	   call tri_f(cell,fbdyy,gy,detai)
 
-!
-!------------------------------------------------
-! si la cellule est au bord, rotation
-!------------------------------------------------
-!
-	 if (fflagt(cell).eq.1) then
-!
-!------------------------------------------------
-! projection onto the zero-at-the-boundary basis functions
-!
-	   call reduc_gn(gx,bu)
-	   call reduc_gn(gy,bv)
-!
-!------------------------------------------------
-!
-	   call dtrsm4(nm-nc-1,amml,bu,dia2)
-	   call dtrsm4(nm-nc-1,amml,bv,dia2)
-!
-!------------------------------------------------
-! back projection
-!
-	   call reduc_inv_gn(gx,bu)
-	   call reduc_inv_gn(gy,bv)
-!
-!-------------------------------------------------
-! cas normal sans rotation
-!-------------------------------------------------
-!
-	   else
-
 	     call dtrsm4(nm,nm,amm,gx,dia1)
 	     call dtrsm4(nm,nm,amm,gy,dia1)
 
@@ -382,17 +650,10 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
 !
 
 	call cal_flu(fbd,uh)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdx(j,face) = fbd(j,face)*norm(1,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdx,1)
 	call cal_flu(fbd,vh)
-	do face=1,nf
-	   do j=1,ngf
-	   fbdy(j,face) = fbd(j,face)*norm(2,face)
-	   enddo
-	enddo
+	call norm_flu(fbd,fbdy,2)
+
 	call massbc(fbdx,fbdy)
 
 
@@ -404,34 +665,54 @@ subroutine cal_var(u0,v0,h0,u1,v1,h1,mu0,mb0,fc,w0,z0,g0,hb,time)
          by    = vect(4,cell)
          detai = vect(5,cell)
 
-           do j=1,nm
-              b(j) =0.d0
-           enddo
+         b =0.d0
+         gx = uh(:,cell)
+         gy = vh(:,cell)
 
-	   do j=1,nm
-	      ak(j) = uh(j,cell)
-	   enddo
-	   coeff = by*detai
-	   call cal_b(b,ak,agx,coeff)
-	   coeff =-ay*detai
-	   call cal_b(b,ak,agy,coeff)
+         tk=pbdl_inv(cell)
 
-	   do j=1,nm
-	      ak(j) = vh(j,cell)
-	   enddo
-	   coeff =-bx*detai
-	   call cal_b(b,ak,agx,coeff)
-	   coeff = ax*detai
-	   call cal_b(b,ak,agy,coeff)
+!------------------------------------------------
+! element sur le bord --- element courbe
+!------------------------------------------------
+       if (tk.gt.0) then
 
+	 amm1=amm_bd(:,:,tk)
+	 amm2=agx_bd(:,:,tk)
+	 amm3=agy_bd(:,:,tk)
 
-	   call tri_f(cell,fbdx,b,-detai)
+        do k=1,nm
+         dia4(k) = 1.d0/amm1(k,k)
+        enddo
 
-	   call dtrsm4(nm,nm,amm,b,dia1)
+         call cal_b(b,gx,amm2, by*detai)
+         call cal_b(b,gx,amm3,-ay*detai)
 
-	   do j=1,nm
-		h1(j,cell) = b(j)
-	   enddo
+         call cal_b(b,gy,amm2,-bx*detai)
+         call cal_b(b,gy,amm3, ax*detai)
+
+         call tri_fc(cell,fbdx,b,-detai)
+
+         call dtrsm4(nm,nm,amm1,b,dia4)
+	 
+	 
+!------------------------------------------------
+! interior element
+!------------------------------------------------
+	 else
+
+          call cal_b(b,gx,agx, by*detai)
+          call cal_b(b,gx,agy,-ay*detai)
+
+          call cal_b(b,gy,agx,-bx*detai)
+          call cal_b(b,gy,agy, ax*detai)
+
+          call tri_f(cell,fbdx,b,-detai)
+
+	  call dtrsm4(nm,nm,amm,b,dia1)
+
+	 endif
+	  
+	 h1(:,cell)=b
 
 	enddo
 
